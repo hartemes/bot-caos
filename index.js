3753837040ebad51998cf2bc8d8887a3baa87f44
hartemes <65870488+hartemes@users.noreply.github.com>
2026-02-22 16:48:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
+const chromium = require('@sparticuz/chromium');
+const puppeteer = require('puppeteer-core');
 
 const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-gpu'
-    ],
-    headless: true
+    args: chromium.args,
+    executablePath: async () => await chromium.executablePath(),
+    headless: chromium.headless
   }
 });
 
